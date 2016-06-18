fec4ffcac53e22077c875af0996c81749465fa60
yanghdx <yanghdx@163.com>
2016-06-18 14:23:13 +0800

    just set username

--- shirodemo/src/main/java/cn/doit123/shirodemo/core/shiro/SysUserFilter.java
+++ shirodemo/src/main/java/cn/doit123/shirodemo/core/shiro/SysUserFilter.java
@@ -5,22 +5,18 @@ import javax.servlet.ServletResponse;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.web.filter.PathMatchingFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import cn.doit123.shirodemo.services.sysuser.SysUserService;
+import cn.doit123.shirodemo.Constants;
 
 public class SysUserFilter extends PathMatchingFilter {
 
-	@Autowired
-	private SysUserService userService;
-
+	
 	@Override
 	protected boolean onPreHandle(ServletRequest request,
 			ServletResponse response, Object mappedValue) throws Exception {
 
 		String username = (String) SecurityUtils.getSubject().getPrincipal();
-		request.setAttribute("CURRENT_USER",
-				userService.findByUsername(username));
+		request.setAttribute(Constants.CURRENT_USER, username);
 		return true;
 	}
 }
